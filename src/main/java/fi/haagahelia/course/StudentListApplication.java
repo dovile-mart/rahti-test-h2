eb66d6d6bd4c3d0154507ab35ad0d18de80ac594
Martinonyte Dovile <71691245+dovile-mart@users.noreply.github.com>
2025-02-18 11:34:58 +0200

    Testing H2

--- src/main/java/fi/haagahelia/course/StudentListApplication.java
+++ src/main/java/fi/haagahelia/course/StudentListApplication.java
@@ -18,15 +18,15 @@ public class StudentListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentListApplication.class, args);
 	}
-  
+ 
 	@Bean
 	public CommandLineRunner studentDemo(StudentRepository repository) {
 		return (args) -> {
 			log.info("save a couple of students");
-			repository.save(new Student("Aku", "Johnson", "john@h2.com"));
-			repository.save(new Student("Ankka", "Kateson", "kate@h2.com"));	
+			repository.save(new Student("Aku", "Johnson", "john@john.com"));
+			repository.save(new Student("Ankka", "Kateson", "kate@kate.com"));	
 			
-			repository.save(new Student("Musta", "Naamio", "mm@h2.org"));
+			repository.save(new Student("Musta", "Naamio", "mm@bengali.org"));
 			
 			log.info("fetch all students");
 			for (Student student : repository.findAll()) {
